cc9af732f1cb47482f0178a1ee9a0d7e2baa33cd
libra guest <ssylvia@esri.com>
2012-08-02 11:21:04 -0400

    first commit

---
+++ javascript/layout.js
@@ -0,0 +1,316 @@
+$(document).ready(function(e) {
+	  $("#legendToggle").click(function(){
+		if ($("#legendDiv").css('display')=='none'){
+		  $("#legTogText").html('MAP LEGEND ▲');
+		}
+		else{
+		  $("#legTogText").html('MAP LEGEND ▼');
+		}
+		$("#legendDiv").slideToggle();
+	  });
+    })
+
+var popupOpen = false,
+	 j = 0,
+    _storyData = [],
+    selection, mapPoint;
+
+var checkImg = function(obj){
+	
+	var img = new RegExp (/(?:.jpe?g|.gif|.png)/i);
+	var url = new RegExp (/http/i);
+	
+	for (var prop in obj){
+		if (typeof(obj[prop]) == 'string'){
+			if (img.test((obj[prop])) == true && url.test((obj[prop])) == true){
+				return(obj[prop]);
+			}
+		}
+	}
+}
+var findTitle = function(obj){
+	
+	var udrScr = new RegExp (/"_"/i);
+	var url = new RegExp (/http/i);
+	
+	var title = null;
+	for (var prop in obj){
+		if (typeof(obj[prop]) == 'string'){
+			if (title == null && udrScr.test((obj[prop])) == false && (obj[prop]).length > 1 && url.test((obj[prop])) == false){
+				title = obj[prop];
+			}
+		}
+	}
+	return (title);
+}
+
+function findLayers(layers,index){
+	var csvLayers = [];
+	dojo.forEach(layers,function(layer,i){
+		if(layer.id.search("csv") == 0 && layer.visibility == true){
+			dojo.forEach(layer.featureCollection.layers,function(lyr,j){
+				csvLayers.push(lyr.layerObject);
+			});
+		}
+		else if(layer.id.search("featColl") == 0 && layer.visibility == true){
+			dojo.forEach(layer.featureCollection.layers,function(lyr,j){
+				csvLayers.push(lyr.layerObject);
+			});
+		}
+	});
+	_csvLayers[index] = csvLayers;
+	generateGraphics(index);
+}
+
+function generateGraphics(index){
+	var shaded = true
+	dojo.forEach(_csvLayers[index],function(layer,i){
+		if (i < 2){
+			layer.hide();
+			
+			$("#story"+index).append("<div id='story"+index+"group"+i+"' class='group'></div>");
+			
+			var sp = new esri.layers.GraphicsLayer();
+			map.addLayer(sp);
+			_storyPoints[index] = sp;
+			
+			if (layer.graphics[0].attributes.order){
+				layer.graphics.sort(function(a,b){
+					return a.attributes.order - b.attributes.order;
+				});
+			}
+			
+			dojo.forEach(layer.graphics,function(graphic,j){
+				if (j < 99){
+					if (shaded == true){
+						shaded = false;
+						$("#story"+index+"group"+i).append("<div id='story"+index+"group"+i+"point"+j+"' class='storyPoint' title='Click for more information'></div>");
+					}
+					else{
+						shaded = true;
+						$("#story"+index+"group"+i).append("<div id='story"+index+"group"+i+"point"+j+"' class='storyPoint storyPointShaded' title='Click for more information '></div>");
+					}
+					
+					$("#story"+index+"group"+i+"point"+j).data("attributes", graphic.attributes);
+					$("#story"+index+"group"+i+"point"+j).data("geometry", graphic.geometry);
+					$("#story"+index+"group"+i+"point"+j).data("infoWindow", layer.infoTemplate);
+					
+					var group;
+					var fileChange;
+					if (i == 2){
+						group = 'green';
+						fileChange = '';
+					}
+					else if (i == 1){
+						group = 'blue';
+						fileChange = 'b';
+					}
+					else{
+						group = 'red';
+						fileChange = '';
+					}
+						
+					var pt = graphic.geometry;
+					var attr = graphic.attributes;
+					var sym = new esri.symbol.PictureMarkerSymbol("images/icons/"+group+"/NumberIcon"+fileChange+(j+1)+".png", 22, 28).setOffset(3,8);
+					var info = layer.infoTemplate;
+					
+					if (graphic.attributes.order){
+						$("#story"+index+"group"+i+"point"+j).append("<div id='story"+index+"group"+i+"indexCon"+j+"' class='indexCon "+group+"'><div id='story"+index+"group"+i+"pointIndex"+j+"' class='pointIndex'>"+graphic.attributes.order+"</div></div>");
+					}
+					else{
+						$("#story"+index+"group"+i+"point"+j).append("<div id='story"+index+"group"+i+"indexCon"+j+"' class='indexCon "+group+"'><img id='story"+index+"group"+i+"pointIndex"+j+"' class='pointIndex' src='images/icons/"+group+"/NumberIcon"+fileChange+(j+1)+".png'></div>");
+					}
+					/*
+					else{
+						$("#story"+index+"group"+i+"point"+j).append("<div id='story"+index+"group"+i+"indexCon"+j+"' class='indexCon "+group+"'><div id='story"+index+"group"+i+"pointIndex"+j+"' class='pointIndex'>"+(j+1)+"</div></div>");
+					}
+					*/
+					
+					var image = graphic.attributes.img || checkImg(graphic.attributes);
+					var ifImg = true;
+							
+					if (image != null && image != "http://www.landscope.org/_res/img/contentTypeThumbnails/articleSmall.png"){
+						$("#story"+index+"group"+i+"point"+j).append("<div id='story"+index+"group"+i+"imgCon"+j+"' class='imgCon'><img id='story"+index+"group"+i+"img"+j+"' class='spImg' src='"+image+"' alt=''></div>");
+					}
+					else{
+						ifImg = false;
+					}
+					
+					var title = graphic.attributes.title || findTitle(graphic.attributes);
+					
+					$("#story"+index+"group"+i+"point"+j).data("title", title);
+							
+					if (title != null){
+						$("#story"+index+"group"+i+"point"+j).append("<div id='story"+index+"group"+i+"textCon"+j+"' class='textCon'><p id='story"+index+"group"+i+"titleText"+j+"' class='titleText'>"+title+"</p></div>");
+						
+						var textWidth;
+						
+						if (ifImg == true){
+							textWidth = (($(".storyPoint").width()) - 115);
+							$("#story"+index+"group"+i+"textCon"+j).css('left',115);
+						}
+						else{
+							textWidth = (($(".storyPoint").width()) - 35);
+							$("#story"+index+"group"+i+"textCon"+j).css('left',35);
+						}
+						$("#story"+index+"group"+i+"textCon"+j).width(textWidth);
+					}
+					
+					attr.displayTitle = title;
+					
+					sp.add(new esri.Graphic(pt,sym,attr,info));
+				
+				}
+				
+			});
+		}
+	});
+
+	
+	startUpListeners(index);
+	
+	$(".titleText").ellipsis();
+}	
+
+function startUpListeners(index){
+	dojo.connect(_storyPoints[index],"onMouseOver",function(){
+		_maps[index].setCursor('pointer');
+		event.graphic.setSymbol(event.graphic.symbol.setHeight(34).setWidth(27).setOffset(3,10));
+	});
+	dojo.connect(_storyPoints[index],"onMouseOut",function(){
+		_maps[index].setCursor('default');
+		event.graphic.setSymbol(event.graphic.symbol.setHeight(28).setWidth(22).setOffset(3,8));
+	});
+	dojo.connect(_maps[index],"onClick",function(){
+		popupOpen = true;
+	});
+	dojo.connect(_popup[index],"onHide",function(){
+		popupOpen = false;
+		_popup[index].clearFeatures();
+	});
+	 dojo.connect(_storyPoints[index], "onClick", function(event) {
+        var scrollTop = ($("#storyPoints").scrollTop());
+        $(".storyPoint").removeClass("selection");
+        dojo.forEach(_storyData, function(sp) {
+            if (event.graphic.attributes == sp.data('attributes')) {
+                selection = sp;
+                if (sp.position().top < 0) {
+                    var pos = $("#storyPoints").scrollTop() + sp.position().top;
+                    $("#storyPoints").scrollTop(pos);
+                }
+                else if (sp.position().top >= $("#storyPoints").height()) {
+                    var pos = $("#storyPoints").scrollTop() + sp.position().top - $("#storyPoints").height() + sp.height();
+                    $("#storyPoints").scrollTop(pos);
+                }
+            }
+        });
+    });
+	$(".storyPoint").mouseover(function(e) {
+        $(".storyPoint").removeClass('active');
+		$(this).addClass('active');
+    });
+	$(".storyPoint").click(function(e) {
+        popupOpen = true;
+		_popup[index].show($(this).data('geometry'));
+		if(_maps[j].extent.contains($(this).data('geometry')) == false){
+			panMap($(this).data('geometry'));
+		}
+		var attr = $(this).data('attributes');
+		dojo.forEach(_storyPoints,function (lyr,idx){
+			dojo.forEach(lyr.graphics,function(graphic,ix){
+				if (graphic.attributes == attr){
+					_popup[index].setFeatures([graphic]);
+				}
+			});
+		});
+    });
+	$("#storyPoints").mouseout(function(e) {
+        $(".storyPoint").removeClass('active');
+    });
+	
+	$(".tab").click(function(e) {
+        j = $(this).data("count");
+		$(".tab").removeClass('selectedStory');
+		$(this).addClass('selectedStory');
+		$(".mapDiv").hide();
+		$(".story").hide();
+		$(".legendDiv").hide();
+		$("#mapDiv"+j).show();
+		$("#story"+j).show();
+		$("#legendDiv"+j).show();
+		dojo.byId("title").innerHTML = configOptions.title || _mapInfo[j].title || "";
+        dojo.byId("subtitle").innerHTML = configOptions.subtitle|| _mapInfo[j].subtitle || "";
+		dojo.byId("description").innerHTML = configOptions.description|| _mapInfo[j].description || "";
+    });
+}
+
+function panMap(pt){
+	var ext = _maps[j].extent;
+	var height = ext.getHeight();
+	var width = ext.getWidth();
+	if (pt.x < ext.xmin && pt.y > ext.ymax){
+		var xmin = (pt.x - (width/5));
+		var xmax = (pt.x - (width/5) + width);
+		var ymax = (pt.y + (height/5));
+		var ymin =(pt.y + (height/5) - height);
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+	else if (pt.x > ext.xmax && pt.y > ext.ymax){
+		var xmin = (pt.x + (width/5) - width);
+		var xmax = (pt.x + (width/5));
+		var ymax = (pt.y + (height/5));
+		var ymin =(pt.y + (height/5) - height);
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+	else if (pt.x > ext.xmax && pt.y < ext.ymin){
+		var xmin = (pt.x + (width/5) - width);
+		var xmax = (pt.x + (width/5));
+		var ymax = (pt.y - (height/5) + height);
+		var ymin =(pt.y - (height/5));
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+	else if (pt.x < ext.xmin && pt.y < ext.ymin){
+		var xmin = (pt.x - (width/5));
+		var xmax = (pt.x - (width/5) + width);
+		var ymax = (pt.y - (height/5) + height);
+		var ymin =(pt.y - (height/5));
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+	else if (pt.x < ext.xmin){
+		var xmin = (pt.x - (width/5));
+		var xmax = (pt.x - (width/5) + width);
+		var ymax = ext.ymax;
+		var ymin = ext.ymin;
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+	else if (pt.x > ext.xmax){
+		var xmin = (pt.x + (width/5) - width);
+		var xmax = (pt.x + (width/5));
+		var ymax = ext.ymax;
+		var ymin = ext.ymin;
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+	else if (pt.y > ext.ymax){
+		var xmin = ext.xmin;
+		var xmax = ext.xmax;
+		var ymax = (pt.y + (height/5));
+		var ymin =(pt.y + (height/5) - height);
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+	else if (pt.y < ext.ymin){
+		var xmin = ext.xmin;
+		var xmax = ext.xmax;
+		var ymax = (pt.y - (height/5) + height);
+		var ymin =(pt.y - (height/5));
+		_maps[j].setExtent(new esri.geometry.Extent({"xmin":xmin,"ymin":ymin,"xmax":xmax,"ymax":ymax,
+  "spatialReference":{"wkid":102100}}));
+	}
+}
---
+++ javascript/map.js
@@ -0,0 +1,241 @@
+dojo.require("esri.map");
+dojo.require("esri.dijit.Legend");
+dojo.require("esri.dijit.Scalebar");
+dojo.require("esri.arcgis.utils");
+dojo.require("esri.IdentityManager");
+dojo.require("dijit.dijit"); // optimize: load dijit layer
+dojo.require("dijit.layout.BorderContainer");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("esri.dijit.Popup");
+
+var map, _maps = [], _popup = [], _mapInfo = [], urlObject, _csvLayers = [], _storyPoints = [], mapsLoaded = 0, mapsReady = false;
+
+function initMap() {
+      patchID();
+      
+      if(configOptions.geometryserviceurl && location.protocol === "https:"){
+        configOptions.geometryserviceurl = configOptions.geometryserviceurl.replace('http:','https:');
+      }
+      esri.config.defaults.geometryService = new esri.tasks.GeometryService(configOptions.geometryserviceurl);  
+      
+
+
+      if(!configOptions.sharingurl){
+        configOptions.sharingurl = location.protocol + '//' + location.host + "/sharing/content/items";
+      }
+      esri.arcgis.utils.arcgisUrl = configOptions.sharingurl;
+       
+      if(!configOptions.proxyurl){   
+        configOptions.proxyurl = location.protocol + '//' + location.host + "/sharing/proxy";
+      }
+
+      esri.config.defaults.io.proxyUrl =  configOptions.proxyurl;
+
+      esri.config.defaults.io.alwaysUseProxy = false;
+      
+      urlObject = esri.urlToObject(document.location.href);
+      urlObject.query = urlObject.query || {};
+      
+      if(urlObject.query.title){
+        configOptions.title = urlObject.query.title;
+      }
+      if(urlObject.query.subtitle){
+        configOptions.title = urlObject.query.subtitle;
+      }
+      if(urlObject.query.webmap){
+        configOptions.webmap = urlObject.query.webmap;      
+      } 
+      if(urlObject.query.bingMapsKey){
+        configOptions.bingmapskey = urlObject.query.bingMapsKey;      
+      }
+	  
+   	 createMaps(); 
+}
+
+
+function createMaps(){
+		
+	dojo.forEach(configOptions.webmaps,function(webmap,i){
+		
+	  $("#map").append("<div id='mapDiv"+i+"' class='mapDiv'></div>");
+	  $("#storyPoints").append("<div id='story"+i+"' class='story'></div>");
+	  $("#tabs").append("<div id='tab"+i+"' class='tab'></div>");
+	  $("#legendDiv").append("<div id='legendDiv"+i+"' class='legendDiv'></div>");
+	  
+	  $("#tab"+i).data("count", i);
+	  
+	  var popup = new esri.dijit.Popup({
+		highlight:true
+      }, dojo.create("div"));
+	  
+	  _popup[i] = popup;
+
+      var mapDeferred = esri.arcgis.utils.createMap(webmap.id, "mapDiv"+i, {
+        mapOptions: {
+          slider: true,
+          nav: false,
+          wrapAround180:true,
+		  infoWindow:popup
+        },
+        ignorePopups:false,
+        bingMapsKey: configOptions.bingmapskey
+      });
+
+      mapDeferred.addCallback(function (response) {
+		  
+        mapInfo = {"title": response.itemInfo.item.title || "",
+				   "subtitle": response.itemInfo.item.snippet || "",
+				   "description": response.itemInfo.item.description || ""};
+        
+		$("#tab"+i).html(mapInfo.title);
+		
+        map = response.map;
+		_maps[i] = map;
+		_mapInfo[i] = mapInfo;
+		
+		dojo.connect(map,"onUpdateEnd",hideLoader);
+		  
+        var layers = response.itemInfo.itemData.operationalLayers;
+		
+        if(map.loaded){
+          initUI(layers, i);
+		  findLayers(layers,i);
+        }
+        else{
+          dojo.connect(map,"onLoad",function(){
+            initUI(layers, i);
+			findLayers(layers,i);
+          });
+        }
+        //resize the map when the browser resizes
+        dojo.connect(dijit.byId('map'), 'resize', map,map.resize);
+       });
+
+      mapDeferred.addErrback(function (error) {
+          alert("Unable to create map: " + " " + dojo.toJson(error.message));
+      });
+	});
+}
+
+
+    function initUI(layers, i) {
+      //add chrome theme for popup
+      dojo.addClass(map.infoWindow.domNode, "chrome");
+      //add the scalebar 
+      var scalebar = new esri.dijit.Scalebar({
+        map: map,
+        scalebarUnit:"english" //metric or english
+      });
+
+      var layerInfo = buildLayersList(layers);      
+
+      
+      if(layerInfo.length > 0){
+        var legendDijit = new esri.dijit.Legend({
+          map:map,
+          layerInfos:layerInfo
+        },"legendDiv"+i);
+        legendDijit.startup();
+      }
+      else{
+        dojo.byId('legendDiv'+i).innerHTML = '';
+      }
+	}
+	
+	function buildLayersList(layers){
+      //build a list of layers for the legend.
+      var layerInfos = [];
+      dojo.forEach(layers, function(mapLayer, index) {
+		if(mapLayer.id.search("csv") != 0) {
+			if(mapLayer.featureCollection){
+			  if (mapLayer.featureCollection.layers && mapLayer.featureCollection.showLegend) {
+				if(mapLayer.featureCollection.layers.length === 1){
+				  layerInfos.push({"layer":mapLayer.featureCollection.layers[0].layerObject,"title":mapLayer.title});
+				}
+				else{
+				  dojo.forEach(mapLayer.featureCollection.layers, function(layer) {
+					 layerInfos.push({
+						layer: layer.layerObject, 
+						title: layer.layerDefinition.name
+					  });
+				  }); 
+				}
+				
+			  }
+			 }else if (mapLayer.layerObject){
+				layerInfos.push({layer:mapLayer.layerObject, title:mapLayer.title});
+			 }
+		}
+      });
+			return layerInfos;
+}
+	
+function hideLoader(){
+	if (mapsLoaded <= configOptions.webmaps.length){
+		if (mapsLoaded == configOptions.webmaps.length - 1){
+			mapsReady = true;
+			mapsLoaded++
+			document.title = configOptions.title|| _mapInfo[0].title || "";
+        	dojo.byId("title").innerHTML = configOptions.title || _mapInfo[0].title || "";
+        	dojo.byId("subtitle").innerHTML = configOptions.subtitle|| _mapInfo[0].subtitle || "";
+			dojo.byId("description").innerHTML = configOptions.description|| _mapInfo[0].description || "";
+			$("#tab0").addClass('selectedStory');
+			$("#legendDiv0").show();
+			dojo.forEach(configOptions.webmaps,function(map,i){
+				if (i != 0){
+					$("#mapDiv"+i).hide();
+				}
+			});
+			$("#mapBlind").fadeOut();
+			$("#loadingCon").hide();
+		}
+		else{
+			mapsLoaded++
+		}
+	}
+}
+
+
+function patchID() {  //patch id manager for use in apps.arcgis.com
+       esri.id._isIdProvider = function(server, resource) {
+       // server and resource are assumed one of portal domains
+ 
+       var i = -1, j = -1;
+ 
+       dojo.forEach(this._gwDomains, function(domain, idx) {
+         if (i === -1 && domain.regex.test(server)) {
+           i = idx;
+         }
+         if (j === -1 && domain.regex.test(resource)) {
+           j = idx;
+         }
+       });
+ 
+       var retVal = false;
+   
+       if (i > -1 && j > -1) {
+         if (i === 0 || i === 4) {
+           if (j === 0 || j === 4) {
+             retVal = true;
+           }
+         }
+         else if (i === 1) {
+           if (j === 1 || j === 2) {
+             retVal = true;
+           }
+         }
+         else if (i === 2) {
+           if (j === 2) {
+             retVal = true;
+           }
+         }
+         else if (i === 3) {
+           if (j === 3) {
+             retVal = true;
+           }
+         }
+       }
+ 
+       return retVal;
+     };    
+    }
